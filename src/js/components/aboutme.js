0dbb6f3db5f6b36f4c58485efd36bcd17ed4aa5e
Ragemouse <rasmus.krister.moberg@gmail.com>
2019-08-01 14:35:57 +0200

    typos

--- src/js/components/aboutme.js
+++ src/js/components/aboutme.js
@@ -21,23 +21,23 @@ class AboutText extends React.Component{
             <div>
               <h1>About me</h1>
                 <article className="pb-4">
-                      <div className="pb-4 leading-loose text-shadow responsive-text">
-                        My name is Rasmus Moberg, a junior web developer currently studying the fine arts
-                        of javaScript. I'm also the owner of a freelance business currently available for hiring.
-                        I've created this website for you - Take a look around! If you like what you see; I'm available as of now
-                         for internship later in October.
-                     </div>
-                     <div className="pb-4 leading-loose text-shadow responsive-text">
-                        To describe myself as a programmer I would say that I'm
-                        knowledgeable and thorough for my level of experience and that I'm always looking for the best way
-                        to solve any problem.
-                        JavaScript is my main language and has so far gotten the highest grades
-                        in all courses. I am however learning other languages to broaden my programmatic knowledge, I'm always up for a challenge.
-                     </div>
-                     <div className="pb-4 leading-loose text-shadow responsive-text">
-                        As a person I would say the most defining things is that I'm empathetic, love to just talk about anything and
-                        to try new things.
-                      </div>
+                  <div className="pb-4 leading-loose text-shadow responsive-text">
+                    My name is Rasmus Moberg, a junior web developer currently studying the fine arts
+                    of javaScript. I'm also the owner of a freelance business should the need arise.
+                    I've created this website for you - Take a look around! If you like what you see; I'm available as of now
+                    for internship later in October.
+                 </div>
+                 <div className="pb-4 leading-loose text-shadow responsive-text">
+                    To describe myself as a programmer I would say that I'm
+                    knowledgeable and thorough for my level of experience and that I'm always looking for the best way
+                    to solve any problem.
+                    JavaScript is my main language and has so far gotten the highest grades
+                    in all courses. I'm also learning other languages to broaden my programmatic knowledge, I'm always up for a challenge.
+                 </div>
+                 <div className="pb-4 leading-loose text-shadow responsive-text">
+                    As a person I would say the most defining things is that I'm empathetic, love to just talk about anything and
+                    to try new things.
+                  </div>
                 </article>
                 <article>
                   <h1>About this page</h1>
@@ -50,8 +50,8 @@ class AboutText extends React.Component{
                     </div>
                     <div className="pb-4 leading-loose text-shadow responsive-text">
                       You can check out the code for this website <a href="https://github.com/ragemous3/rasmus-moberg-cv" className="underline"> here</a>.
-                      This site have a pretty great polyfill-cover for most browsers. Older browsers and Edge
-                      will have a more jerky scrolling and will performe less smoothly.
+                      This site have a pretty great polyfill-cover for most browsers. Older browsers, Edge and some
+                      safari will not have a smooth scroll experience.
                     </div>
                 </article>
               </div>
